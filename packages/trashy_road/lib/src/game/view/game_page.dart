311d1bc2e078e4531656f75a4352a258ee04775c
alestiago <dev@alestiago.com>
2024-02-02 17:03:15 +0000

    chore: set-up GamePage

---
+++ packages/trashy_road/lib/src/game/view/game_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatelessWidget {
+  const GamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

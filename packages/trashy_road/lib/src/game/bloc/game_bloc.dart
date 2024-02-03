c9a787dba18a4c41c45884760138e03fe4350b8c
alestiago <dev@alestiago.com>
2024-02-03 09:12:03 +0000

    feat: defined GameState

--- packages/trashy_road/lib/src/game/bloc/game_bloc.dart
+++ packages/trashy_road/lib/src/game/bloc/game_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:js_interop';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
@@ -6,12 +8,7 @@ part 'game_event.dart';
 part 'game_state.dart';
 
 class GameBloc extends Bloc<GameEvent, GameState> {
-  GameBloc()
-      : super(
-          GameState(
-            status: GameStatus.playing,
-          ),
-        ) {
+  GameBloc() : super(const GameState.initial()) {
     on<GameEvent>((event, emit) {
       // TODO: implement event handler
     });

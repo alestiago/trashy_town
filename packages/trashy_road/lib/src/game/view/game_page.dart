40f83a8f387007864c4ed41babfce0d3a7b4cd78
alestiago <dev@alestiago.com>
2024-02-03 09:33:29 +0000

    feat: include simple InventoryHud

--- packages/trashy_road/lib/src/game/view/game_page.dart
+++ packages/trashy_road/lib/src/game/view/game_page.dart
@@ -2,26 +2,40 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trashy_road/src/game/game.dart';
+import 'package:trashy_road/src/game/widgets/widgets.dart';
 
 class GamePage extends StatelessWidget {
   const GamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO(alestiago): Consider refactoring, if the [GameBloc] doesn't end up
-    // being consumed by other widgets in the tree.
-
     return BlocProvider(
       create: (context) => GameBloc(),
-      child: Builder(
-        builder: (context) {
-          final gameBloc = context.read<GameBloc>();
+      child: const _GameView(),
+    );
+  }
+}
 
-          return GameWidget.controlled(
-            gameFactory: () => TrashyRoadGame(gameBloc: gameBloc),
-          );
-        },
-      ),
+class _GameView extends StatelessWidget {
+  const _GameView();
+
+  @override
+  Widget build(BuildContext context) {
+    final gameBloc = context.read<GameBloc>();
+
+    return Stack(
+      children: [
+        const Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: InventoryHud(),
+          ),
+        ),
+        GameWidget.controlled(
+          gameFactory: () => TrashyRoadGame(gameBloc: gameBloc),
+        ),
+      ],
     );
   }
 }
---
+++ packages/trashy_road/lib/src/game/widgets/inventory_hud.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trashy_road/src/game/game.dart';
+
+/// {@template InventoryHud}
+/// A widget that displays the current trash count.
+/// {@endtemplate}
+class InventoryHud extends StatelessWidget {
+  /// {@macro InventoryHud}
+  const InventoryHud({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border.all(width: 2),
+        color: const Color(0xFFFFFFFF),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: BlocSelector<GameBloc, GameState, int>(
+        selector: (state) {
+          return state.inventory.trash;
+        },
+        builder: (context, trash) {
+          return Text(
+            trash.toString(),
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF000000),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

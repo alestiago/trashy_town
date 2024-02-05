f27057acb0ac0d64920971dd6a76cdf1f969ae41
alestiago <dev@alestiago.com>
2024-02-05 08:13:13 +0000

    chore: removed utils directory

--- tool/tiled_map_parser/lib/src/tiled_map_parser.dart
+++
@@ -1,7 +0,0 @@
-/// {@template tiled_map_parser}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class TiledMapParser {
-  /// {@macro tiled_map_parser}
-  const TiledMapParser();
-}

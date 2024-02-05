2c03baa5a0ebea372760582a003e785643aa3b02
alestiago <dev@alestiago.com>
2024-02-05 22:13:34 +0000

    refactor: tiled_map_parser

--- tool/tiled_map_parser/lib/main.dart
+++ tool/tiled_map_parser/lib/main.dart
@@ -1,5 +1,80 @@
 import 'dart:io';
+import 'package:mason_logger/mason_logger.dart';
+import 'package:path/path.dart' as path;
 import 'package:xml/xml.dart';
+
+/// The file extension used by Tiled to identify a map file.
+const _tiledMapExtension = '.tmx';
+
+class _TiledObject {
+  const _TiledObject({
+    required String template,
+    required XmlElement element,
+  })  : _element = element,
+        _template = template;
+
+  static _TiledObject? tryParse(XmlElement element) {
+    final template = element.getAttribute('template');
+    if (template == null) {
+      return null;
+    }
+
+    return _TiledObject(
+      template: template,
+      element: element,
+    );
+  }
+
+  final String _template;
+
+  final XmlElement _element;
+
+  _TiledTemplateFile _templateFile(String basePath) =>
+      _TiledTemplateFile.fromFile(
+        File(
+          path.join(basePath, _template),
+        ),
+      );
+
+  void addTemplateData(String basePath) {
+    final templateFile = _templateFile(basePath);
+
+    final object = templateFile._object;
+    final newAttributes = object.attributes.where(
+      (attribute) {
+        print('name: ${attribute.name} value: ${attribute.value}');
+        print('element: ${_element.getAttribute(attribute.name.toString())}');
+        return _element.getAttribute(attribute.name.toString()) == null;
+      },
+    );
+
+    for (final attribute in newAttributes) {
+      _element.setAttribute(attribute.name.toString(), attribute.value);
+    }
+  }
+}
+
+class _TiledTemplateFile {
+  const _TiledTemplateFile(this._object);
+
+  static _TiledTemplateFile fromFile(File file) {
+    if (!file.existsSync()) {
+      throw Exception('File does not exist: ${file.path}');
+    }
+
+    final contents = file.readAsStringSync();
+    final document = XmlDocument.parse(contents);
+    final object = document.findAllElements('object').firstOrNull;
+
+    if (object == null) {
+      throw Exception('No object found in file: ${file.path}');
+    }
+
+    return _TiledTemplateFile(object);
+  }
+
+  final XmlElement _object;
+}
 
 // TODO(OlliePugh): The documentation here should follow
 // EffectiveDart convention:
@@ -8,42 +83,44 @@ import 'package:xml/xml.dart';
 /// tiled doesnt parse template information properly
 /// this function will copy all template data into
 /// any objects
+///
+/// This script should be run from `tool/tiled_map_parser`, otherwise it will
+/// throw an error.
 void main() async {
-// Get the system temp directory.
-  final dir = Directory('assets/tiles/'); // loop through dir
+  final logger = Logger();
 
-  dir
-      .list(recursive: true, followLinks: false)
-      .listen((FileSystemEntity entity) async {
-    final extension = ".${entity.path.split('.').last}";
+  final scriptPath = path.join('tool', 'tiled_map_parser');
+  if (!Directory.current.path.endsWith(scriptPath)) {
+    throw Exception(
+      'This script should be run from the `$scriptPath` directory',
+    );
+  }
 
-    if (extension != '.tmx') {
-      return; // if extension isnt .tmx we don't want to modify
+  final assetsDirectory = Directory(
+    path.join('..', '..', 'packages', 'trashy_road', 'assets', 'tiles'),
+  );
+
+  final tmxFiles = assetsDirectory
+      .listSync()
+      .whereType<File>()
+      .where((file) => path.extension(file.path) == _tiledMapExtension)
+      .toSet();
+
+  logger.info('Found ${tmxFiles.length} .tmx files');
+
+  for (final file in tmxFiles) {
+    final tmxFile = XmlDocument.parse(file.readAsStringSync());
+    final objects = tmxFile.findAllElements('object').toList();
+    logger.info('Found ${objects.length} objects in ${file.path}');
+
+    final templates =
+        objects.map(_TiledObject.tryParse).whereType<_TiledObject>();
+    logger.info('Found ${templates.length} templates in ${file.path}');
+
+    for (final template in templates) {
+      template.addTemplateData(assetsDirectory.path);
     }
-    final file = File(entity.path);
-    final document = XmlDocument.parse(file.readAsStringSync());
 
-    document.findAllElements('objectgroup').toList().forEach((e) {
-      for (final element in e.children) {
-        final template = element.getAttribute('template');
-        if (template != null) {
-          // if the object has a template
-          final templateFile = File('assets/tiles/$template');
-          final templateDoc =
-              XmlDocument.parse(templateFile.readAsStringSync());
-          for (final attribute
-              in templateDoc.findAllElements('object').toList()[0].attributes) {
-            final parsedAttribute = attribute.toString().split('=');
-
-            element.setAttribute(
-              parsedAttribute[0],
-              parsedAttribute[1].replaceAll('"', ''),
-            );
-          }
-        }
-      }
-    });
-
-    file.writeAsStringSync(document.toXmlString());
-  });
+    file.writeAsStringSync(tmxFile.toXmlString(pretty: true));
+  }
 }

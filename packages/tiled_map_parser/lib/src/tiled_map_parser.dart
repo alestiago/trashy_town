85b87f0d1a57cc431aefc305af121cdd1501ed7f
alestiago <dev@alestiago.com>
2024-02-05 08:09:56 +0000

    chore: created package with Very Good CLI

---
+++ packages/tiled_map_parser/lib/src/tiled_map_parser.dart
@@ -0,0 +1,7 @@
+/// {@template tiled_map_parser}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class TiledMapParser {
+  /// {@macro tiled_map_parser}
+  const TiledMapParser();
+}
---
+++ packages/tiled_map_parser/test/src/tiled_map_parser_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:tiled_map_parser/tiled_map_parser.dart';
+
+void main() {
+  group('TiledMapParser', () {
+    test('can be instantiated', () {
+      expect(TiledMapParser(), isNotNull);
+    });
+  });
+}

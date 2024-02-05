f1f52aeee7f0e1d357a45727332905e879ffd255
alestiago <dev@alestiago.com>
2024-02-05 23:23:38 +0000

    refactor: finish documenting and modifying script

--- tool/tiled_map_parser/lib/main.dart
+++ tool/tiled_map_parser/lib/main.dart
@@ -1,3 +1,13 @@
+// TODO(OlliePugh): Add an issue on FlameTiled, with reproductive steps to Flame
+// and add the issue URL here.
+
+/// {@template tile_map_parser}
+///
+/// This script should be run from `tool/tiled_map_parser`, otherwise it will
+/// throw an error.
+/// {@endtemplate}
+library tiled_map_parser;
+
 import 'dart:io';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:path/path.dart' as path;
@@ -6,6 +16,8 @@ import 'package:xml/xml.dart';
 /// The file extension used by Tiled to identify a map file.
 const _tiledMapExtension = '.tmx';
 
+/// Represents a Tiled object, within a Tiled map file (`.tmx`) that uses a
+/// template.
 class _TiledObject {
   const _TiledObject({
     required String template,
@@ -13,6 +25,12 @@ class _TiledObject {
   })  : _element = element,
         _template = template;
 
+  /// Attempts to parse a [_TiledObject] from an [XmlElement].
+  ///
+  /// It expects an element with the format:
+  /// ```xml
+  /// <object template="path/to/template.tx" ... />
+  /// ```
   static _TiledObject? tryParse(XmlElement element) {
     final template = element.getAttribute('template');
     if (template == null) {
@@ -25,10 +43,13 @@ class _TiledObject {
     );
   }
 
+  /// The relative path to the template file (`.tx`) that this object uses.
   final String _template;
 
+  /// The raw XML element that represents this object.
   final XmlElement _element;
 
+  /// Gets the template file (`.tx`) from the given base path.
   _TiledTemplateFile _templateFile(String basePath) =>
       _TiledTemplateFile.fromFile(
         File(
@@ -36,6 +57,41 @@ class _TiledObject {
         ),
       );
 
+  /// Copies data found in the template file (`.tx`) to the object element in
+  /// the `.tmx` file.
+  ///
+  /// If the object element already has an attribute or property, it will not be
+  /// overwritten.
+  ///
+  /// The data that is copied are the attributes and properties of the template
+  /// file.
+  ///
+  /// For example:
+  /// ```xml
+  /// <!-- map.tmx -->
+  /// <object id="32" template="road_lane.tx" x="64" y="160" ... >
+  /// ```
+  /// ```xml
+  /// <!-- road_lane.tx -->
+  /// <?xml version="1.0" encoding="UTF-8"?>
+  /// <template>
+  ///  <object name="road_lane" class="road_lane">
+  ///   <properties>
+  ///    <property name="speed" type="int" value="0"/>
+  ///   </properties>
+  ///  <point/>
+  ///  </object>
+  /// </template>
+  /// ```
+  ///
+  /// Will update `map.tmx` to:
+  /// ```xml
+  /// <object id="32" template="road_lane.tx" x="64" y="160"
+  ///         name="road_lane" class="road_lane">
+  ///   <properties>
+  ///     <property name="speed" type="int" value="0"/>
+  ///   </properties>
+  /// </object>
   void addTemplateData(String basePath) {
     final templateFile = _templateFile(basePath);
 
@@ -55,6 +111,7 @@ class _TiledObject {
   }
 }
 
+/// Represents a Tiled template file (`.tx`).
 class _TiledTemplateFile {
   const _TiledTemplateFile({
     required XmlElement object,
@@ -82,29 +139,21 @@ class _TiledTemplateFile {
     );
   }
 
+  /// The first <object> element found in the template file.
   final XmlElement _object;
 
+  /// The first <properties> element found in the template file, if it exists.
   final XmlElement? _properties;
 }
 
-// TODO(OlliePugh): The documentation here should follow
-// EffectiveDart convention:
-// https://dart.dev/guides/language/effective-dart/documentation
-
-/// tiled doesnt parse template information properly
-/// this function will copy all template data into
-/// any objects
-///
-/// This script should be run from `tool/tiled_map_parser`, otherwise it will
-/// throw an error.
+/// {@macro tile_map_parser}
 void main() async {
   final logger = Logger();
 
   final scriptPath = path.join('tool', 'tiled_map_parser');
   if (!Directory.current.path.endsWith(scriptPath)) {
-    throw Exception(
-      'This script should be run from the `$scriptPath` directory',
-    );
+    logger.err('This script should be run from the `$scriptPath` directory');
+    return;
   }
 
   final assetsDirectory = Directory(
@@ -121,15 +170,14 @@ void main() async {
 
   for (final file in tmxFiles) {
     final tmxFile = XmlDocument.parse(file.readAsStringSync());
-    final objects = tmxFile.findAllElements('object').toList();
+
+    final objectsXmlElement = tmxFile.findAllElements('object').toList();
+    final objects =
+        objectsXmlElement.map(_TiledObject.tryParse).whereType<_TiledObject>();
     logger.info('Found ${objects.length} objects in ${file.path}');
 
-    final templates =
-        objects.map(_TiledObject.tryParse).whereType<_TiledObject>();
-    logger.info('Found ${templates.length} templates in ${file.path}');
-
-    for (final template in templates) {
-      template.addTemplateData(assetsDirectory.path);
+    for (final object in objects) {
+      object.addTemplateData(assetsDirectory.path);
     }
 
     file.writeAsStringSync(tmxFile.toXmlString(pretty: true));

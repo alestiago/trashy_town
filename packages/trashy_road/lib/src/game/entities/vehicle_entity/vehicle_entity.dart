d088b478ad66c3f02a7df485a0139f1a7b35c9b0
alestiago <dev@alestiago.com>
2024-02-08 07:28:31 +0000

    chore: minor fixed to VehicleEntity

--- packages/trashy_road/lib/src/game/entities/vehicle_entity/vehicle_entity.dart
+++ packages/trashy_road/lib/src/game/entities/vehicle_entity/vehicle_entity.dart
@@ -6,31 +6,35 @@ import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:trashy_road/src/game/entities/vehicle_entity/behaviours/vehicle_hitting_player_behavior.dart';
 import 'package:trashy_road/src/game/game.dart';
 
-/// Base class for all vehicles
-abstract class VehicleEntity extends PositionedEntity {
+/// A vehicle that moves along a [RoadLane].
+///
+/// See also:
+///
+/// * [VehicleSpawningBehavior], which is used to spawn vehicles on a
+/// [RoadLane].
+class VehicleEntity extends PositionedEntity {
   VehicleEntity({
-    required Vector2 size,
-    required this.roadLane,
+    required ShapeHitbox hitbox,
+    required RoadLane roadLane,
     required super.children,
-  }) : super(
-          position: roadLane.position.clone(),
+  })  : _roadLane = roadLane,
+        super(
           anchor: Anchor.topLeft,
+          position: roadLane.position.clone(),
           behaviors: [
-            PropagatingCollisionBehavior(
-              RectangleHitbox(
-                size: size,
-                isSolid: true,
-              ),
-            ),
+            PropagatingCollisionBehavior(hitbox),
             VehicleHittingPlayerBehavior(),
           ],
         );
 
   @override
-  FutureOr<void> onLoad() {
-    if (roadLane.direction == RoadLaneDirection.leftToRight) flipHorizontally();
-    return super.onLoad();
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
+
+    if (_roadLane.direction == RoadLaneDirection.leftToRight) {
+      flipHorizontally();
+    }
   }
 
-  final RoadLane roadLane;
+  final RoadLane _roadLane;
 }

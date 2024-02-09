1fe695d2b4a2b02975beaedf4c3466f82924a9c8
alestiago <dev@alestiago.com>
2024-02-09 22:19:25 +0000

    feat: reset vehicles as soon as they reach the bound

--- packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart
+++ packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 
+import 'package:flame/components.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:trashy_road/src/game/game.dart';
 
 /// {@template VehicleSpawningBehavior}
 /// Spawns vehicles in the road lane.
 /// {@endtemplate}
-class VehicleSpawningBehavior extends Behavior<RoadLane> {
+class VehicleSpawningBehavior extends Behavior<RoadLane> with HasGameReference {
   /// {@macro VehicleSpawningBehavior}
   VehicleSpawningBehavior();
 
@@ -17,5 +18,19 @@ class VehicleSpawningBehavior extends Behavior<RoadLane> {
     // TODO(alestiago): Intead of adding once, add vehicles at intervals.
     final vehicle = Bus(roadLane: parent);
     parent.add(vehicle);
+  }
+
+  @override
+  void update(double dt) {
+    final world = ancestors().whereType<TrashyRoadWorld>().first;
+    final bounds = world.bounds;
+
+    final vehicles = parent.children.whereType<Vehicle>();
+    for (final vehicle in vehicles) {
+      final isWithinBound = bounds.isPointInside(vehicle.position);
+      if (!isWithinBound) {
+        vehicle.position = vehicle.roadLane.position.clone();
+      }
+    }
   }
 }

c20a34db2e9de6fb3849f361b0a36450e32453b4
alestiago <dev@alestiago.com>
2024-02-09 22:20:09 +0000

    chore: removed unused reference

--- packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart
+++ packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
 
-import 'package:flame/components.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:trashy_road/src/game/game.dart';
 
 /// {@template VehicleSpawningBehavior}
 /// Spawns vehicles in the road lane.
 /// {@endtemplate}
-class VehicleSpawningBehavior extends Behavior<RoadLane> with HasGameReference {
+class VehicleSpawningBehavior extends Behavior<RoadLane> {
   /// {@macro VehicleSpawningBehavior}
   VehicleSpawningBehavior();
 

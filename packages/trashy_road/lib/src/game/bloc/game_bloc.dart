fe79a1f80d576906f9fb7ab8358dfe05cb7eaf11
Alejandro Santiago <dev@alestiago.com>
2024-02-09 19:19:50 +0000

    feat: reset the game after the player gets run over (#50)
    * feat: define VehicleDrivingBehavior
    
    * chore: include bus in RoadLane
    
    * feat: add  VehicleDrivingBehavior
    
    * feat: add logic to reset the game

--- packages/trashy_road/lib/src/game/bloc/game_bloc.dart
+++ packages/trashy_road/lib/src/game/bloc/game_bloc.dart
@@ -7,7 +7,24 @@ part 'game_state.dart';
 
 class GameBloc extends Bloc<GameEvent, GameState> {
   GameBloc() : super(const GameState.initial()) {
+    on<GameReadyEvent>(_onGameReady);
+    on<GameInteractedEvent>(_onGameInteraction);
     on<GameCollectedTrashEvent>(_onCollectedTrash);
+    on<GameResetEvent>(_onGameReset);
+  }
+
+  void _onGameReady(
+    GameReadyEvent event,
+    Emitter<GameState> emit,
+  ) {
+    emit(state.copyWith(status: GameStatus.ready));
+  }
+
+  void _onGameInteraction(
+    GameInteractedEvent event,
+    Emitter<GameState> emit,
+  ) {
+    emit(state.copyWith(status: GameStatus.playing));
   }
 
   void _onCollectedTrash(
@@ -18,5 +35,14 @@ class GameBloc extends Bloc<GameEvent, GameState> {
       trash: state.inventory.trash + 1,
     );
     emit(state.copyWith(inventory: inventory));
+  }
+
+  void _onGameReset(
+    GameResetEvent event,
+    Emitter<GameState> emit,
+  ) {
+    emit(
+      const GameState.initial().copyWith(status: GameStatus.resetting),
+    );
   }
 }

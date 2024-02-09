1c8a3554040d6bb6a2b78ed5b3d086fea384118c
Alejandro Santiago <dev@alestiago.com>
2024-02-09 22:56:14 +0000

    Update packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart

--- packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart
+++ packages/trashy_road/lib/src/game/entities/road_lane/behavior/vehicle_spawning_behavior.dart
@@ -30,7 +30,7 @@ class VehicleSpawningBehavior extends Behavior<RoadLane> {
       final isWithinBound =
           bounds.isPointInside(parent.position + vehicle.position);
       if (!isWithinBound) {
-        vehicle.position = Vector2.zero();
+        vehicle.position.setAll(0)
       }
     }
   }

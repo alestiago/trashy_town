ad03e219947d08db5533bb0bc53d15d231eae4d2
Ollie Pugh <oliver.pugh@mac.com>
2024-02-14 12:08:56 +0000

    fix: some event were tied to fps (#101)

--- packages/trashy_road/lib/src/game/entities/vehicle/behaviors/vehicle_driving_behavior.dart
+++ packages/trashy_road/lib/src/game/entities/vehicle/behaviors/vehicle_driving_behavior.dart
@@ -1,16 +1,17 @@
 import 'package:flame_behaviors/flame_behaviors.dart';
-import 'package:trashy_road/game_settings.dart';
 import 'package:trashy_road/src/game/game.dart';
 
 class VehicleDrivingBehavior extends Behavior<Vehicle> {
   VehicleDrivingBehavior();
 
+  /// The speed multiplier for the car.
+  static const carSpeedMultiplier = 80;
+
   @override
   void update(double dt) {
     super.update(dt);
 
-    final distanceCovered =
-        parent.roadLane.speed * GameSettings.carSpeedMultiplier;
+    final distanceCovered = parent.roadLane.speed * carSpeedMultiplier * dt;
     final direction =
         parent.roadLane.direction == RoadLaneDirection.leftToRight ? 1 : -1;
     parent.position.x += distanceCovered * direction;

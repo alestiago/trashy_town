98ab3873c4a0200e7c19b39ac5dc16b37549dd6a
alestiago <dev@alestiago.com>
2024-02-15 22:38:05 +0000

    refactor: remove BlocBuilder from GamePage

--- packages/trashy_road/lib/src/game/view/game_page.dart
+++ packages/trashy_road/lib/src/game/view/game_page.dart
@@ -1,16 +1,10 @@
-import 'dart:math';
-
-import 'package:flame/game.dart' hide Route;
 import 'package:flame_tiled/flame_tiled.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trashy_road/src/game/game.dart';
 import 'package:trashy_road/src/loading/loading.dart';
 import 'package:trashy_road/src/pause/pause.dart';
 import 'package:trashy_road/src/score/view/view.dart';
-
-final _random = Random(0);
 
 class GamePage extends StatelessWidget {
   const GamePage({
@@ -40,82 +34,45 @@ class GamePage extends StatelessWidget {
   }
 }
 
-class _GameView extends StatefulWidget {
+class _GameView extends StatelessWidget {
   const _GameView();
 
   @override
-  State<_GameView> createState() => _GameViewState();
-}
-
-class _GameViewState extends State<_GameView> {
-  TrashyRoadGame? _game;
-
-  @override
   Widget build(BuildContext context) {
-    final loadingBloc = context.read<PreloadCubit>();
     final gameBloc = context.read<GameBloc>();
-
-    TrashyRoadGame gameBuilder() {
-      return kDebugMode
-          ? DebugTrashyRoadGame(
-              gameBloc: gameBloc,
-              images: loadingBloc.images,
-              random: _random,
-            )
-          : TrashyRoadGame(
-              gameBloc: gameBloc,
-              images: loadingBloc.images,
-              random: _random,
-            );
-    }
-
-    _game ??= gameBuilder();
 
     return BlocListener<GameBloc, GameState>(
       listenWhen: (previous, current) => current.status == GameStatus.completed,
       listener: (context, state) {
         Navigator.pushReplacement(context, ScorePage.route());
       },
-      child: BlocBuilder<GameBloc, GameState>(
-        buildWhen: (previous, current) {
-          return previous.status == GameStatus.playing &&
-              current.status == GameStatus.resetting;
-        },
-        builder: (context, state) {
-          if (state.status == GameStatus.resetting) {
-            _game = gameBuilder();
-            gameBloc.add(const GameReadyEvent());
-          }
-
-          return Stack(
-            children: [
-              GameWidget(game: _game!),
-              const Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: EdgeInsets.all(8),
-                  child: InventoryHud(),
-                ),
+      child: Stack(
+        children: [
+          const TrashyRoadGameWidget(),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: InventoryHud(),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: PauseButton(
+                onPause: () {
+                  gameBloc.add(const GamePausedEvent());
+                  return true;
+                },
+                onResume: () {
+                  gameBloc.add(const GameResumedEvent());
+                  return true;
+                },
               ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: PauseButton(
-                    onPause: () {
-                      gameBloc.add(const GamePausedEvent());
-                      return true;
-                    },
-                    onResume: () {
-                      gameBloc.add(const GameResumedEvent());
-                      return true;
-                    },
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
--- packages/trashy_road/lib/src/game/widgets/widgets.dart
+++ packages/trashy_road/lib/src/game/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'inventory_hud.dart';
+export 'trashy_road_game_widget.dart';

372864923dc3960a16f0d2b60a6880a0c46d84a1
alestiago <dev@alestiago.com>
2024-02-17 16:45:24 +0000

    docs: updated documentation

--- packages/trashy_road/lib/src/game/entities/map_edge/map_edge.dart
+++ packages/trashy_road/lib/src/game/entities/map_edge/map_edge.dart
@@ -6,10 +6,36 @@ import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:tiled/tiled.dart';
 import 'package:trashy_road/src/game/game.dart';
 
-/// A border around the map.
+/// An untraversable rectangle.
 ///
-/// Blocks the [Player] from leaving the map.
+/// There are usually four [MapEdge]s in a map, one for each side. They delimit
+/// the area where the [Player] can move around.
+///
+/// Visually, it adds a semi-transparent black rectangle to the map. Which makes
+/// it easier to see the boundaries of the map, since it makes those tiles
+/// directly beneath it darker.
+///
+/// See also:
+///
+/// * [Untraversable], which marks a component as untraversable.
+/// * [PlayerObstacleBehavior], which makes the player unable to traverse
+///  through an [Untraversable] component.
 class MapEdge extends PositionedEntity with Untraversable {
+  MapEdge._({required super.position, required super.size})
+      : super(
+          anchor: Anchor.topLeft,
+        ) {
+    addAll([
+      PropagatingCollisionBehavior(
+        RectangleHitbox(size: size),
+      ),
+      RectangleComponent(size: size)
+        ..setColor(
+          const Color.fromARGB(64, 0, 0, 0),
+        ),
+    ]);
+  }
+
   /// Derives a [MapEdge] from a [TiledObject].
   factory MapEdge.fromTiledObject(TiledObject tiledObject) {
     return MapEdge._(
@@ -17,25 +43,4 @@ class MapEdge extends PositionedEntity with Untraversable {
       size: Vector2(tiledObject.width, tiledObject.height),
     );
   }
-  MapEdge._({required super.position, required super.size})
-      : super(
-          anchor: Anchor.topLeft,
-        ) {
-    addAll([
-      PropagatingCollisionBehavior(
-        RectangleHitbox(
-          size: size,
-        ),
-      ),
-      RectangleComponent(size: size)
-        ..setColor(
-          const Color.fromARGB(_opaqueness, 0, 0, 0),
-        ),
-    ]);
-  }
-
-  /// The opaqueness of the border.
-  ///
-  /// 0 is fully transparent, 255 is fully opaque.
-  static const int _opaqueness = 64;
 }
--- packages/trashy_road/lib/src/game/entities/player/behaviors/player_obstacle_behavior.dart
+++ packages/trashy_road/lib/src/game/entities/player/behaviors/player_obstacle_behavior.dart
@@ -3,7 +3,7 @@ import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:trashy_road/src/game/game.dart';
 
 /// Communicates to the [PlayerKeyboardMovingBehavior] that the player
-/// has collided with an [Obstacle].
+/// has collided with an [Untraversable] component.
 class PlayerObstacleBehavior extends CollisionBehavior<Untraversable, Player> {
   @override
   void onCollisionStart(

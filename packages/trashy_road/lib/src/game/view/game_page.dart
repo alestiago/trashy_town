10993cf27d69e1ac4587727095c348ade61a3642
alestiago <dev@alestiago.com>
2024-02-17 07:21:56 +0000

    feat: complete map when game is completed

--- packages/trashy_road/lib/src/game/view/game_page.dart
+++ packages/trashy_road/lib/src/game/view/game_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trashy_road/src/game/game.dart';
 import 'package:trashy_road/src/loading/loading.dart';
+import 'package:trashy_road/src/maps/maps.dart';
 import 'package:trashy_road/src/pause/pause.dart';
 import 'package:trashy_road/src/score/view/view.dart';
 
@@ -56,6 +57,12 @@ class _GameView extends StatelessWidget {
     return BlocListener<GameBloc, GameState>(
       listenWhen: (previous, current) => current.status == GameStatus.completed,
       listener: (context, state) {
+        context.read<GameMapsBloc>().add(
+              GameMapCompletedEvent(
+                identifier: state.identifier,
+                score: state.score,
+              ),
+            );
         Navigator.pushReplacement(context, ScorePage.route());
       },
       child: Stack(

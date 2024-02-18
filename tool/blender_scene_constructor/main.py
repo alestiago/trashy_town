4dc122cea7d3e82d51e226e59b21ff9524b10932
Ollie Pugh <oliver.pugh@mac.com>
2024-02-18 20:51:26 +0000

    feat: update blender script to soften shadows

--- tool/blender_scene_constructor/main.py
+++ tool/blender_scene_constructor/main.py
@@ -13,7 +13,7 @@ OBJECT_COLLECTION_NAME = "Subject"
 # ----------------------------------------------------
 
 # Set this to True if you want to render only the shadows
-ONLY_RENDER_SHADOWS = False
+ONLY_RENDER_SHADOWS = True
 
 def setup_camera(camera):
     # Set the camera to orthographic mode
@@ -27,46 +27,62 @@ def setup_camera(camera):
 
     # Set the rotation on the X-axis to 45 degrees (converted to radians)
     camera.rotation_euler[0] = math.radians(CAMERA_ANGLE)
-
+    
 # Function to add a plane and set it as a shadow catcher
-def add_shadow_catcher_plane():
+def add_shadow_catcher_plane(): 
     # Check if a shadow catcher plane with the specific name already exists
     if "ShadowCatcherPlane" not in bpy.data.objects:
-        bpy.ops.mesh.primitive_plane_add(size=10, location=(0, 0, 0))
+        bpy.ops.mesh.primitive_plane_add(size=30, location=(0, 0, 0))
         plane = bpy.context.object
-        plane.cycles.is_shadow_catcher = True
+        plane.is_shadow_catcher = True
         plane.name = "ShadowCatcherPlane"
+        # Move the plane to the scene collection
+        try:
+            bpy.context.scene.collection.objects.link(plane)
+            # Unlink from the Subject collection if it exists
+            if bpy.data.collections.get("Subject"):
+                bpy.data.collections["Subject"].objects.unlink(plane)
+        except:
+            pass
 
-# Function to add or move the sun lamp
-def add_or_move_sun(camera):
-    # Check if a sun lamp already exists
-    sun = bpy.data.lights.get("Sun")
-    
-    if sun is None:
-        # Add a new sun lamp
-        bpy.ops.object.light_add(type='SUN', location=(camera.location.x - 3, camera.location.y, camera.location.z))
-        sun = bpy.context.object
-        sun.name = "Sun"
-        
-    sun = bpy.context.scene.objects["Sun"]
-    
-    # Set the strength of the sun lamp to 2
-    sun.data.energy = 2
-    
-    # Position the sun 3 units to the left of the camera and at the same Y position
-    sun.location.x = camera.location.x - SHADOW_SIZE
-    sun.location.y = camera.location.y
-    sun.location.z = camera.location.z
-    
-    # Calculate the direction vector from sun location to origin
-    direction = -sun.location
-    # Align the sun lamp to point in the calculated direction
-    sun.rotation_euler = direction.to_track_quat('-Z', 'Y').to_euler()
+# Function to delete all sun lamps
+def delete_all_sun_lamps():
+    sun_lamps = [obj for obj in bpy.context.scene.objects if obj.type == 'LIGHT' and obj.data.type == 'SUN']
+    for sun in sun_lamps:
+        bpy.data.objects.remove(sun, do_unlink=True)
 
+# Function to add or move the area lamp
+def add_or_move_area(camera):
+    # Delete all sun lamps
+    delete_all_sun_lamps()
+    area = bpy.data.objects.get("Area")
+    
+    if area is None:
+        # Add a new area lamp
+        bpy.ops.object.light_add(type='AREA', location=(camera.location.x - 3, camera.location.y, camera.location.z))
+        # Fetch the newly created area lamp
+        area = bpy.context.object
+        area.name = "Area"
+    
+    # Set the strength of the area lamp to 2
+    area.data.energy = 100
+    
+    # Position the area lamp 3 units to the left of the camera and at the same Y position
+    area.location.x = camera.location.x - SHADOW_SIZE
+    area.location.y = camera.location.y
+    area.location.z = camera.location.z
+    
+    # Calculate the direction vector from area lamp location to origin
+    direction = -area.location
+    # Align the area lamp to point in the calculated direction
+    area.rotation_euler = direction.to_track_quat('-Z', 'Y').to_euler()
+    
 # Set render engine to Cycles
 bpy.context.scene.render.engine = 'CYCLES'
 
 def setup_only_render_shadows(only_render_shadows=True):
+    plane = bpy.data.objects["ShadowCatcherPlane"]
+    plane.hide_render = not only_render_shadows
     # Create a new material
     material = bpy.data.materials.new(name="ShadowOnlyMaterial")
 
@@ -132,7 +148,7 @@ def setup_only_render_shadows(only_render_shadows=True):
 if camera is not None:
     setup_camera(camera)
     add_shadow_catcher_plane()
-    add_or_move_sun(camera)
+    add_or_move_area(camera)
     setup_only_render_shadows(ONLY_RENDER_SHADOWS)
     
 else:

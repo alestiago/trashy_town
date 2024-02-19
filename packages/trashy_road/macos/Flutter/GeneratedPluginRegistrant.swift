41c4190235c2e5847a604199222ef69be3a91f86
Alejandro Santiago <dev@alestiago.com>
2024-02-19 18:32:58 +0000

    chore: install audio dependencies (#162)

--- packages/trashy_road/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/trashy_road/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import path_provider_foundation
 import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }

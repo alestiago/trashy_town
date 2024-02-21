658c5b21ee41710e6eec6eae18f716d24c2117de
alestiago <dev@alestiago.com>
2024-02-21 23:40:51 +0000

    refactor: rely on trash constructors

--- packages/trashy_road/lib/src/game/entities/entities.dart
+++ packages/trashy_road/lib/src/game/entities/entities.dart
@@ -6,8 +6,6 @@ export 'obstacle/obstacle.dart';
 export 'player/player.dart';
 export 'road_lane/road_lane.dart';
 export 'trash/trash.dart';
-export 'trash/trash_glass.dart';
-export 'trash/trash_plastic.dart';
 export 'trash_can/trash_can.dart';
 export 'trash_can/trash_can_glass.dart';
 export 'trash_can/trash_can_plastic.dart';

6f9b9fbf95ab9093e6db6afeecd5f8da823c6e24
Ollie Pugh <oliver.pugh@mac.com>
2024-02-24 10:47:28 +0000

    feat: add inventory status bar (#192)
    * feat: add inventory status bar
    
    * refactor: inventory hud
    
    ---------
    
    Co-authored-by: alestiago <dev@alestiago.com>

--- packages/trashy_road/lib/src/game/widgets/inventory_hud.dart
+++ packages/trashy_road/lib/src/game/widgets/inventory_hud.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trashy_road/gen/gen.dart';
 import 'package:trashy_road/src/game/game.dart';
 
 /// {@template InventoryHud}
@@ -18,21 +19,90 @@ class InventoryHud extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8),
-        child: BlocSelector<GameBloc, GameState, int>(
+        child: BlocSelector<GameBloc, GameState, List<TrashType>>(
           selector: (state) {
-            return state.inventory.items.length;
+            return state.inventory.items;
           },
           builder: (context, trash) {
-            return DefaultTextStyle(
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF000000),
-              ),
-              child: Text('Trash: $trash'),
+            return Row(
+              children: [
+                _TrashTypeCounter.organic(trash: trash),
+                _TrashTypeCounter.paper(trash: trash),
+                _TrashTypeCounter.plastic(trash: trash),
+                _TrashTypeCounter.glass(trash: trash),
+              ],
             );
           },
         ),
+      ),
+    );
+  }
+}
+
+class _TrashTypeCounter extends StatelessWidget {
+  const _TrashTypeCounter._({
+    required this.amount,
+    required this.imagePath,
+  });
+
+  factory _TrashTypeCounter.organic({
+    required List<TrashType> trash,
+  }) {
+    return _TrashTypeCounter._(
+      amount: trash.where((type) => type == TrashType.organic).length,
+      imagePath: Assets.images.appleCore2.path,
+    );
+  }
+
+  factory _TrashTypeCounter.glass({
+    required List<TrashType> trash,
+  }) {
+    return _TrashTypeCounter._(
+      amount: trash.where((type) => type == TrashType.glass).length,
+      imagePath: Assets.images.plasticBottle1.path,
+    );
+  }
+
+  factory _TrashTypeCounter.plastic({
+    required List<TrashType> trash,
+  }) {
+    return _TrashTypeCounter._(
+      amount: trash.where((type) => type == TrashType.plastic).length,
+      imagePath: Assets.images.plasticBottle1.path,
+    );
+  }
+
+  factory _TrashTypeCounter.paper({
+    required List<TrashType> trash,
+  }) {
+    return _TrashTypeCounter._(
+      amount: trash.where((type) => type == TrashType.paper).length,
+      imagePath: Assets.images.paper1.path,
+    );
+  }
+
+  /// The amount of trash.
+  final int amount;
+
+  /// The image path for the trash type.
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: Row(
+        children: [
+          DefaultTextStyle(
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF000000),
+            ),
+            child: Text(amount.toString()),
+          ),
+          Image.asset(imagePath, width: 48, height: 48),
+        ],
       ),
     );
   }

3dd535612d9b3ab48527e6f17147cdd4d98df5df
Ollie Pugh <oliver.pugh@mac.com>
2024-02-28 20:22:02 +0000

    feat: add lane types

--- packages/trashy_road/lib/src/game/entities/vehicle/vehicle.dart
+++ packages/trashy_road/lib/src/game/entities/vehicle/vehicle.dart
@@ -1,9 +1,29 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
+import 'package:meta/meta.dart';
 import 'package:trashy_road/src/game/game.dart';
 
 export 'behaviors/behaviors.dart';
+
+/// The different types of [Vehicle].
+enum VehicleType {
+  redCar._('car_red'),
+  blueCar._('car_blue'),
+  yellowCar._('car_yellow');
+
+  const VehicleType._(this.name);
+
+  /// The [name] of the vehicle type in the Tiled map.
+  final String name;
+
+  @internal
+  static VehicleType? tryParse(String value) => _valueToEnumMap[value];
+
+  static final _valueToEnumMap = <String, VehicleType>{
+    for (final value in VehicleType.values) value.name: value,
+  };
+}
 
 /// A vehicle that moves along a [RoadLane].
 ///

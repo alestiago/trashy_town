48af95e987d150b1309e4780b66d57d61092bcca
alestiago <dev@alestiago.com>
2024-03-06 23:10:36 +0000

    chore: sorted directories

--- packages/trashy_road/lib/src/game/widgets/widgets.dart
+++ packages/trashy_road/lib/src/game/widgets/widgets.dart
@@ -2,6 +2,6 @@ export 'animated_star.dart';
 export 'game_stopwatch.dart';
 export 'inventory_hud.dart';
 export 'pause_button.dart';
-export 'top_hud.dart';
 export 'playing_hud_transition.dart';
+export 'top_hud.dart';
 export 'trashy_road_game_widget.dart';

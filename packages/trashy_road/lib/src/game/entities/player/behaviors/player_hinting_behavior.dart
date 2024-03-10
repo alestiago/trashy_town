3612c3c7469c9950ccf60db400a0be54b5f9c446
Alejandro Santiago <dev@alestiago.com>
2024-03-10 18:26:50 +0000

    feat: hint only when playing (#297)
    * feat: hint only when playing
    
    * refactor: adjusted logic

--- packages/trashy_road/lib/src/game/entities/player/behaviors/player_hinting_behavior.dart
+++ packages/trashy_road/lib/src/game/entities/player/behaviors/player_hinting_behavior.dart
@@ -5,6 +5,7 @@ import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/extensions.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
+import 'package:flame_bloc/flame_bloc.dart';
 import 'package:flutter/animation.dart';
 import 'package:trashy_road/game_settings.dart';
 import 'package:trashy_road/gen/gen.dart';
@@ -17,7 +18,8 @@ import 'package:trashy_road/src/game/game.dart';
 /// ([_hintDelay]) and a certain amount of time ([_hintInterval]) has passed
 /// since the last hint was shown.
 /// {@endtemplate}
-class PlayerHintingBehavior extends Behavior<Player> with HasGameReference {
+class PlayerHintingBehavior extends Behavior<Player>
+    with HasGameReference, FlameBlocReader<GameBloc, GameState> {
   /// The minimum time between hints.
   static const _hintInterval = 5.0;
 
@@ -105,6 +107,10 @@ class PlayerHintingBehavior extends Behavior<Player> with HasGameReference {
       // If there is no trash, no hint is needed and thus, the behavior can be
       // removed.
       removeFromParent();
+      return;
+    }
+
+    if (bloc.state.status != GameStatus.playing) {
       return;
     }
 

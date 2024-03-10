b4c9b42220e1204e953bfe46750f970578d7ce25
alestiago <dev@alestiago.com>
2024-03-10 18:10:43 +0000

    chore: remove unused import

--- packages/trashy_road/lib/src/game/bloc/audio/audio_cubit.dart
+++ packages/trashy_road/lib/src/game/bloc/audio/audio_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flame_audio/bgm.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'audio_state.dart';

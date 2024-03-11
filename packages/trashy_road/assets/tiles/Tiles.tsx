63a3657ba6293bc5e1baee33a30fcb57b7742d79
Ollie Pugh <oliver.pugh@mac.com>
2024-03-11 00:11:38 +0000

    feat: add padding and bleed to reduce ghost lines

--- packages/trashy_road/assets/tiles/Tiles.tsx
+++ packages/trashy_road/assets/tiles/Tiles.tsx
@@ -1,14 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.10.2" name="Tiles" tilewidth="768" tileheight="1024" tilecount="33" columns="0">
+<tileset version="1.9" tiledversion="1.10.2" name="Tiles" tilewidth="768" tileheight="1024" tilecount="20" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
   <image width="128" height="128" source="../images/sprites/apple_core_1.png"/>
  </tile>
  <tile id="3">
   <image width="128" height="128" source="../images/tiled/plastic_trash_can.png"/>
- </tile>
- <tile id="7">
-  <image width="128" height="64" source="../images/sprites/grass-flowers.png"/>
  </tile>
  <tile id="9">
   <image width="300" height="300" source="../images/sprites/tree_1.png"/>
@@ -36,42 +33,6 @@
  </tile>
  <tile id="19">
   <image width="512" height="256" source="../images/sprites/bench.png"/>
- </tile>
- <tile id="20">
-  <image width="128" height="64" source="../images/sprites/grass--dirt-west.png"/>
- </tile>
- <tile id="21">
-  <image width="128" height="64" source="../images/sprites/grass--dirt-north.png"/>
- </tile>
- <tile id="22">
-  <image width="128" height="64" source="../images/sprites/grass--dirt-northwest.png"/>
- </tile>
- <tile id="23">
-  <image width="128" height="64" source="../images/sprites/grass.png"/>
- </tile>
- <tile id="24">
-  <image width="128" height="64" source="../images/sprites/road.png"/>
- </tile>
- <tile id="25">
-  <image width="128" height="64" source="../images/sprites/pavement.png"/>
- </tile>
- <tile id="27">
-  <image width="128" height="64" source="../images/sprites/pavement--kirb-south.png"/>
- </tile>
- <tile id="28">
-  <image width="128" height="64" source="../images/sprites/pavement--kirb-south-west.png"/>
- </tile>
- <tile id="29">
-  <image width="128" height="64" source="../images/sprites/dirt.png"/>
- </tile>
- <tile id="30">
-  <image width="128" height="64" source="../images/sprites/pavement--kirb-north.png"/>
- </tile>
- <tile id="31">
-  <image width="128" height="64" source="../images/sprites/pavement--kirb-north-west.png"/>
- </tile>
- <tile id="32">
-  <image width="128" height="64" source="../images/sprites/pavement--kirb-west.png"/>
  </tile>
  <tile id="33">
   <image width="128" height="128" source="../images/sprites/paper_1.png"/>
---
+++ packages/trashy_road/assets/tiles/background-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.10.2" name="background-tiles" tilewidth="128" tileheight="64" spacing="2" tilecount="25" columns="5">
+ <image source="../images/sprites/tilemap.png" width="650" height="330"/>
+</tileset>

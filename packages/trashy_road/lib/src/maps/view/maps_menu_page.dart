ae97806975374b6b954e2a2b5b5e85676f8295bd
alestiago <dev@alestiago.com>
2024-03-11 23:13:18 +0000

    feat: add buildings to MapsMenuPage

--- packages/trashy_road/lib/src/maps/view/maps_menu_page.dart
+++ packages/trashy_road/lib/src/maps/view/maps_menu_page.dart
@@ -1,6 +1,7 @@
 import 'package:basura/basura.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trashy_road/gen/gen.dart';
 import 'package:trashy_road/src/maps/maps.dart';
 
 /// {@template MapsMenuPage}
@@ -34,6 +35,8 @@ class _MapsMenuView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
     return DecoratedBox(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
@@ -45,39 +48,67 @@ class _MapsMenuView extends StatelessWidget {
           ],
         ),
       ),
-      child: BlocSelector<GameMapsBloc, GameMapsState, GameMapsCollection>(
-        selector: (state) => state.maps,
-        builder: (context, maps) {
-          final mapsValues = maps.values.toList();
-          final screenSize = MediaQuery.sizeOf(context);
-
-          const mainCrossAxisExtent = 150.0;
-          const mainAxisSpacing = 20.0;
-
-          final padding = EdgeInsets.symmetric(
-            vertical: mainAxisSpacing * 3,
-            horizontal: screenSize.width * 0.1,
-          );
-
-          return Center(
-            child: SizedBox(
-              width: mainCrossAxisExtent * 5 + padding.horizontal,
-              child: GridView.builder(
-                padding: padding,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: mainCrossAxisExtent,
-                  crossAxisSpacing: 50,
-                  mainAxisSpacing: mainAxisSpacing,
-                ),
-                itemCount: mapsValues.length,
-                itemBuilder: (context, index) {
-                  return GameMapTile(map: mapsValues[index]);
-                },
+      child: Stack(
+        children: [
+          Positioned.fill(
+            top: null,
+            child: Transform.translate(
+              offset: Offset(
+                0,
+                size.width > size.height
+                    ? (size.width - size.height) / ((1920 / 732) * 2)
+                    : 0,
+              ),
+              child: Image.asset(
+                Assets.images.display.playBackgroundHouses.path,
+                fit: BoxFit.cover,
               ),
             ),
-          );
-        },
+          ),
+          const _MapsGrid(),
+        ],
       ),
+    );
+  }
+}
+
+class _MapsGrid extends StatelessWidget {
+  const _MapsGrid();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<GameMapsBloc, GameMapsState, GameMapsCollection>(
+      selector: (state) => state.maps,
+      builder: (context, maps) {
+        final mapsValues = maps.values.toList();
+        final screenSize = MediaQuery.sizeOf(context);
+
+        const mainCrossAxisExtent = 150.0;
+        const mainAxisSpacing = 20.0;
+
+        final padding = EdgeInsets.symmetric(
+          vertical: mainAxisSpacing * 3,
+          horizontal: screenSize.width * 0.1,
+        );
+
+        return Center(
+          child: SizedBox(
+            width: mainCrossAxisExtent * 5 + padding.horizontal,
+            child: GridView.builder(
+              padding: padding,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: mainCrossAxisExtent,
+                crossAxisSpacing: 50,
+                mainAxisSpacing: mainAxisSpacing,
+              ),
+              itemCount: mapsValues.length,
+              itemBuilder: (context, index) {
+                return GameMapTile(map: mapsValues[index]);
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }

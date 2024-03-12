946c3a097dfeda82599b471803849393325ef20b
alestiago <dev@alestiago.com>
2024-03-12 15:19:48 +0000

    fix: experiment, don't cache display images

--- packages/trashy_road/lib/main.dart
+++ packages/trashy_road/lib/main.dart
@@ -35,7 +35,7 @@ class _MyApp extends StatelessWidget {
             images: Images(prefix: ''),
             tiled: TiledCache(),
             imageProviderCache: ImageProviderCache(
-              precacheImage: (provider) => precacheImage(provider, context),
+              precacheImage: (provider) async {},
             ),
           )..loadSequentially(),
         ),

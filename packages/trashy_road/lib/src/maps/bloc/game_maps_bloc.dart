8f1246fa6313134d38eed1348602b5e2a7eb18f0
alestiago <dev@alestiago.com>
2024-03-12 00:01:21 +0000

    feat: add level persitance

--- packages/trashy_road/lib/src/maps/bloc/game_maps_bloc.dart
+++ packages/trashy_road/lib/src/maps/bloc/game_maps_bloc.dart
@@ -1,14 +1,17 @@
+// ignore_for_file: type=lint
+
 import 'dart:collection';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:trashy_road/gen/assets.gen.dart';
 
 part 'game_maps_event.dart';
 part 'game_maps_state.dart';
 
-class GameMapsBloc extends Bloc<GameMapsEvent, GameMapsState> {
+class GameMapsBloc extends HydratedBloc<GameMapsEvent, GameMapsState> {
   GameMapsBloc() : super(GameMapsState.initial()) {
     on<GameMapCompletedEvent>(_onGameCompleted);
   }
@@ -46,5 +49,50 @@ class GameMapsBloc extends Bloc<GameMapsEvent, GameMapsState> {
     if (newMaps != null) {
       emit(state.copyWith(maps: newMaps));
     }
+  }
+
+  @override
+  GameMapsState? fromJson(Map<String, dynamic> json) {
+    final maps = json['maps'] as Map<String, dynamic>;
+    final newMaps = maps.map(
+      (key, value) {
+        value as Map<String, dynamic>;
+
+        final identifier =
+            GameMapIdentifier.values.firstWhere((e) => e.toString() == key);
+        final ratingSteps = value['ratingSteps'] as List<int>;
+        final map = GameMap._(
+          identifier: identifier,
+          displayName: value['displayName'] as String,
+          path: value['path'] as String,
+          score: value['score'] as int?,
+          ratingSteps: (ratingSteps[0], ratingSteps[1], ratingSteps[2]),
+          locked: value['locked'] as bool,
+        );
+        return MapEntry(identifier, map);
+      },
+    );
+    return GameMapsState(maps: UnmodifiableMapView(newMaps));
+  }
+
+  @override
+  Map<String, dynamic>? toJson(GameMapsState state) {
+    return {
+      'maps': {
+        for (final map in state.maps.entries)
+          map.key.toString(): {
+            'identifier': map.value.identifier.toString(),
+            'displayName': map.value.displayName,
+            'path': map.value.path,
+            'score': map.value.score,
+            'ratingSteps': [
+              map.value.ratingSteps.$1,
+              map.value.ratingSteps.$2,
+              map.value.ratingSteps.$3,
+            ],
+            'locked': map.value.locked,
+          },
+      },
+    };
   }
 }
